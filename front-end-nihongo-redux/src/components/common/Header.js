49bea7cf0f5c11877110a25f6c3924b29ffa509d
Thepaut Antoine <thepaut_antoine@outlook.fr>
2021-07-04 10:56:44 +0200

    amélioration du header

--- front-end-nihongo-redux/src/components/common/Header.js
+++ front-end-nihongo-redux/src/components/common/Header.js
@@ -5,71 +5,88 @@ function Header() {
   const activeStyle = { color: "orange" };
 
   return (
-    <nav>
-      <NavLink activeStyle={activeStyle} to="/" exact>
-        Home
-      </NavLink>
+    <>
+      <nav className="navbarMenu">
+        <NavLink activeStyle={activeStyle} to="/" exact>
+          Home
+        </NavLink>
 
-      <NavLink activeStyle={activeStyle} to="/Translation">
-        Translation
-      </NavLink>
+        <NavLink activeStyle={activeStyle} to="/Translation">
+          Translation
+        </NavLink>
 
-      <NavLink activeStyle={activeStyle} to="/kanjis">
-        Kanjis
-      </NavLink>
+        <div className="dropdownMenu">
+          <button className="dropbtn">
+            Japanese characters
+            <i className="fa fa-caret-down"></i>
+          </button>
+          <div className="dropdownMenu-content">
+            <NavLink activeStyle={activeStyle} to="/kanjis">
+              Kanjis
+            </NavLink>
 
-      <NavLink activeStyle={activeStyle} to="/verbs">
-        Verbs
-      </NavLink>
+            <NavLink activeStyle={activeStyle} to="/hiraganas">
+              Hiraganas
+            </NavLink>
 
-      <NavLink activeStyle={activeStyle} to="/naAdjectives">
-        Na-Adjectives
-      </NavLink>
+            <NavLink activeStyle={activeStyle} to="/katakanas">
+              Katakanas
+            </NavLink>
+          </div>
+        </div>
 
-      <NavLink activeStyle={activeStyle} to="/iAdjectives">
-        I-Adjectives
-      </NavLink>
+        <div className="dropdownMenu">
+          <button className="dropbtn">Words</button>
+          <div className="dropdownMenu-content">
+            <NavLink activeStyle={activeStyle} to="/verbs">
+              Verbs
+            </NavLink>
 
-      <NavLink activeStyle={activeStyle} to="/names">
-        Names
-      </NavLink>
+            <NavLink activeStyle={activeStyle} to="/naAdjectives">
+              Na-Adjectives
+            </NavLink>
 
-      <NavLink activeStyle={activeStyle} to="/words">
-        Words
-      </NavLink>
+            <NavLink activeStyle={activeStyle} to="/iAdjectives">
+              I-Adjectives
+            </NavLink>
 
-      <NavLink activeStyle={activeStyle} to="/particules">
-        Particles
-      </NavLink>
+            <NavLink activeStyle={activeStyle} to="/names">
+              Names
+            </NavLink>
 
-      <NavLink activeStyle={activeStyle} to="/sentences">
-        Sentences
-      </NavLink>
+            <NavLink activeStyle={activeStyle} to="/words">
+              Others
+            </NavLink>
+          </div>
+        </div>
+        <div className="dropdownMenu">
+          <button className="dropbtn">Grammar</button>
+          <div className="dropdownMenu-content">
+            <NavLink activeStyle={activeStyle} to="/grammarRules">
+              Grammar Rules
+            </NavLink>
 
-      <NavLink activeStyle={activeStyle} to="/grammarRules">
-        Grammar Rules
-      </NavLink>
+            <NavLink activeStyle={activeStyle} to="/particules">
+              Particles
+            </NavLink>
 
-      <NavLink activeStyle={activeStyle} to="/counters">
-        Counter
-      </NavLink>
+            <NavLink activeStyle={activeStyle} to="/counters">
+              Counter
+            </NavLink>
 
-      <NavLink activeStyle={activeStyle} to="/suffixs">
-        Suffix
-      </NavLink>
-
-      <NavLink activeStyle={activeStyle} to="/hiraganas">
-        Hiraganas
-      </NavLink>
-
-      <NavLink activeStyle={activeStyle} to="/katakanas">
-        Katakanas
-      </NavLink>
-
-      <NavLink activeStyle={activeStyle} to="/about">
-        About
-      </NavLink>
-    </nav>
+            <NavLink activeStyle={activeStyle} to="/suffixs">
+              Suffix
+            </NavLink>
+          </div>
+        </div>
+        <NavLink activeStyle={activeStyle} to="/sentences">
+          Sentences
+        </NavLink>
+        <NavLink activeStyle={activeStyle} to="/about">
+          About
+        </NavLink>
+      </nav>
+    </>
   );
 }
 
