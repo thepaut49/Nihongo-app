7e36e15a1d40b2bec7b613b2502645ad1cf6ec26
Thepaut Antoine <thepaut_antoine@outlook.fr>
2021-07-26 21:47:53 +0200

    Suppression front avec framework flux

--- front-end-nihongo/src/api/apiConstants.js
+++
@@ -1,2 +0,0 @@
-export const REACT_APP_API_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:9090";
--- front-end-nihongo/src/appDispatcher.js
+++
@@ -1,5 +0,0 @@
-import { Dispatcher } from "flux";
-// singleton pattern, only one dispatcher in the application
-const dispatcher = new Dispatcher();
-
-export default dispatcher;
