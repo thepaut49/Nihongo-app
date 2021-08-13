69a225fdae49a3d9f070b33fc41532422fbab4a6
Thepaut Antoine <thepaut_antoine@outlook.fr>
2021-08-13 22:42:06 +0200

    config fin

--- config/defaultConfig.ps1
+++ config/defaultConfig.ps1
@@ -6,9 +6,19 @@ $config = Get-Content config.json | ConvertFrom-Json
 $controllersJava = Get-ChildItem -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller #create list of files
 foreach ($file in $controllersJava)
 {
-    ((Get-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\$file -Raw) -replace $config.host_name,'HOST_NAME') | Set-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\$file
-    ((Get-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\$file -Raw) -replace $config.front_port,'FRONT_PORT') | Set-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\$file
+    If ($file -ne 'user') {
+        ((Get-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\$file -Raw) -replace $config.host_name,'HOST_NAME') | Set-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\$file
+        ((Get-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\$file -Raw) -replace $config.front_port,'FRONT_PORT') | Set-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\$file
+    }
 }
+
+$userControllersJava = Get-ChildItem -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\user #create list of files
+foreach ($file in $userControllersJava)
+{
+    ((Get-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\user\$file -Raw) -replace $config.host_name,'HOST_NAME') | Set-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\user\$file
+    ((Get-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\user\$file -Raw) -replace $config.front_port,'FRONT_PORT') | Set-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\user\$file
+}
+
 
 ((Get-Content -Path '..\apiNihongo\src\main\resources\application.properties' -Raw) -replace $config.db_name,'DB_NAME') | Set-Content -Path '..\apiNihongo\src\main\resources\application.properties'
 ((Get-Content -Path '..\apiNihongo\src\main\resources\application.properties' -Raw) -replace $config.db_username,'DB_USERNAME') | Set-Content -Path '..\apiNihongo\src\main\resources\application.properties'
@@ -16,13 +26,17 @@ foreach ($file in $controllersJava)
 ((Get-Content -Path '..\apiNihongo\src\main\resources\application.properties' -Raw) -replace $config.host_name,'HOST_NAME') | Set-Content -Path '..\apiNihongo\src\main\resources\application.properties'
 ((Get-Content -Path '..\apiNihongo\src\main\resources\application.properties' -Raw) -replace $config.back_port,'BACK_PORT') | Set-Content -Path '..\apiNihongo\src\main\resources\application.properties'
 
-D:\Users\Antoine\Docker\Nihongo-app\apiNihongo\src\main\resources\application.properties
-
 # Modify file in front-end
-((Get-Content -Path '..\front-end-nihongo-redux\keycloak.json' -Raw) -replace $config.host_name,'HOST_NAME') | Set-Content -Path '..\front-end-nihongo-redux\keycloak.json'
 ((Get-Content -Path '..\front-end-nihongo-redux\src\api\apiConstants.js' -Raw) -replace $config.host_name,'HOST_NAME') | Set-Content -Path '..\front-end-nihongo-redux\src\api\apiConstants.js'
 ((Get-Content -Path '..\front-end-nihongo-redux\src\api\apiConstants.js' -Raw) -replace $config.back_port,'BACK_PORT') | Set-Content -Path '..\front-end-nihongo-redux\src\api\apiConstants.js'
 ((Get-Content -Path '..\front-end-nihongo-redux\webpack.config.dev.js' -Raw) -replace $config.host_name,'HOST_NAME') | Set-Content -Path '..\front-end-nihongo-redux\webpack.config.dev.js'
 ((Get-Content -Path '..\front-end-nihongo-redux\webpack.config.dev.js' -Raw) -replace $config.back_port,'BACK_PORT') | Set-Content -Path '..\front-end-nihongo-redux\webpack.config.dev.js'
 ((Get-Content -Path '..\front-end-nihongo-redux\webpack.config.prod.js' -Raw) -replace $config.host_name,'HOST_NAME') | Set-Content -Path '..\front-end-nihongo-redux\webpack.config.prod.js'
 ((Get-Content -Path '..\front-end-nihongo-redux\webpack.config.prod.js' -Raw) -replace $config.back_port,'BACK_PORT') | Set-Content -Path '..\front-end-nihongo-redux\webpack.config.prod.js'
+
+# Modify docker-compose.yml file
+((Get-Content -Path '..\docker-compose.yml' -Raw) -replace $config.host_name,'HOST_NAME') | Set-Content -Path '..\docker-compose.yml'
+((Get-Content -Path '..\docker-compose.yml' -Raw) -replace $config.back_port,'BACK_PORT') | Set-Content -Path '..\docker-compose.yml'
+((Get-Content -Path '..\docker-compose.yml' -Raw) -replace $config.db_name,'DB_NAME') | Set-Content -Path '..\docker-compose.yml'
+((Get-Content -Path '..\docker-compose.yml' -Raw) -replace $config.db_username,'DB_USERNAME') | Set-Content -Path '..\docker-compose.yml'
+((Get-Content -Path '..\docker-compose.yml' -Raw) -replace $config.db_password,'DB_PASSWORD') | Set-Content -Path '..\docker-compose.yml'
--- config/updateConfig.ps1
+++ config/updateConfig.ps1
@@ -6,8 +6,17 @@ $config = Get-Content config.json | ConvertFrom-Json
 $controllersJava = Get-ChildItem -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller #create list of files
 foreach ($file in $controllersJava)
 {
-    ((Get-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\$file -Raw) -replace 'HOST_NAME',$config.host_name) | Set-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\$file
-    ((Get-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\$file -Raw) -replace 'FRONT_PORT',$config.front_port) | Set-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\$file
+    If ($file -ne 'user') {
+        ((Get-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\$file -Raw) -replace 'HOST_NAME',$config.host_name) | Set-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\$file
+        ((Get-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\$file -Raw) -replace 'FRONT_PORT',$config.front_port) | Set-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\$file
+    } 
+}
+
+$userControllersJava = Get-ChildItem -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\user #create list of files
+foreach ($file in $userControllersJava)
+{
+        ((Get-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\user\$file -Raw) -replace 'HOST_NAME',$config.host_name) | Set-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\user\$file
+        ((Get-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\user\$file -Raw) -replace 'FRONT_PORT',$config.front_port) | Set-Content -Path ..\apiNihongo\src\main\java\com\thepaut49\nihongo\controller\user\$file
 }
 
 ((Get-Content -Path '..\apiNihongo\src\main\resources\application.properties' -Raw) -replace 'DB_NAME',$config.db_name) | Set-Content -Path '..\apiNihongo\src\main\resources\application.properties'
@@ -16,9 +25,6 @@ foreach ($file in $controllersJava)
 ((Get-Content -Path '..\apiNihongo\src\main\resources\application.properties' -Raw) -replace 'HOST_NAME',$config.host_name) | Set-Content -Path '..\apiNihongo\src\main\resources\application.properties'
 ((Get-Content -Path '..\apiNihongo\src\main\resources\application.properties' -Raw) -replace 'BACK_PORT',$config.back_port) | Set-Content -Path '..\apiNihongo\src\main\resources\application.properties'
 
-
-D:\Users\Antoine\Docker\Nihongo-app\apiNihongo\src\main\resources\application.properties
-
 # Modify file in front-end
 ((Get-Content -Path '..\front-end-nihongo-redux\src\api\apiConstants.js' -Raw) -replace 'HOST_NAME',$config.host_name) | Set-Content -Path '..\front-end-nihongo-redux\src\api\apiConstants.js'
 ((Get-Content -Path '..\front-end-nihongo-redux\src\api\apiConstants.js' -Raw) -replace 'BACK_PORT',$config.back_port) | Set-Content -Path '..\front-end-nihongo-redux\src\api\apiConstants.js'
@@ -26,3 +32,10 @@ D:\Users\Antoine\Docker\Nihongo-app\apiNihongo\src\main\resources\application.pr
 ((Get-Content -Path '..\front-end-nihongo-redux\webpack.config.dev.js' -Raw) -replace 'BACK_PORT',$config.back_port) | Set-Content -Path '..\front-end-nihongo-redux\webpack.config.dev.js'
 ((Get-Content -Path '..\front-end-nihongo-redux\webpack.config.prod.js' -Raw) -replace 'HOST_NAME',$config.host_name) | Set-Content -Path '..\front-end-nihongo-redux\webpack.config.prod.js'
 ((Get-Content -Path '..\front-end-nihongo-redux\webpack.config.prod.js' -Raw) -replace 'BACK_PORT',$config.back_port) | Set-Content -Path '..\front-end-nihongo-redux\webpack.config.prod.js'
+
+# Modify docker-compose.yml file
+((Get-Content -Path '..\docker-compose.yml' -Raw) -replace 'HOST_NAME',$config.host_name) | Set-Content -Path '..\docker-compose.yml'
+((Get-Content -Path '..\docker-compose.yml' -Raw) -replace 'BACK_PORT',$config.back_port) | Set-Content -Path '..\docker-compose.yml'
+((Get-Content -Path '..\docker-compose.yml' -Raw) -replace 'DB_NAME',$config.db_name) | Set-Content -Path '..\docker-compose.yml'
+((Get-Content -Path '..\docker-compose.yml' -Raw) -replace 'DB_USERNAME',$config.db_username) | Set-Content -Path '..\docker-compose.yml'
+((Get-Content -Path '..\docker-compose.yml' -Raw) -replace 'DB_PASSWORD',$config.db_password) | Set-Content -Path '..\docker-compose.yml'
