cb771b3e39195a4951dbdf7cad19b5d9023779a0
Antoine pc-portable <thepaut_antoine@outlook.fr>
2021-09-28 20:46:05 +0200

    more config

--- config/updateConfig.sh
+++ config/updateConfig.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # read config.json file
 config_file=config.json
-host_name=$(cat $config_file | jq '. | .host_name' | tr -d '"') ;
+front_host_name=$(cat $config_file | jq '. | .front_host_name' | tr -d '"') ;
+back_host_name=$(cat $config_file | jq '. | .back_host_name' | tr -d '"') ;
 db_host=$(cat $config_file | jq '. | .db_host' | tr -d '"');
 db_name=$(cat $config_file | jq '. | .db_name' | tr -d '"');
 db_username=$(cat $config_file | jq '. | .db_username' | tr -d '"');
@@ -10,7 +11,8 @@ back_port=$(cat $config_file | jq '. | .back_port' | tr -d '"');
 front_port=$(cat $config_file | jq '. | .front_port' | tr -d '"');
 secret=$(cat $config_file | jq '. | .secret' | tr -d '"');
 
-echo "host_name   = $host_name";
+echo "front_host_name   = $front_host_name";
+echo "back_host_name   = $back_host_name";
 echo "db_host     = $db_host";
 echo "db_name     = $db_name";
 echo "db_username = $db_username";
@@ -37,7 +39,7 @@ echo "BACK_DIR   = $BACK_DIR";
 # Modify file in back-end
 cd "$BACK_DIR/src/main/java/com/thepaut49/nihongo/controller"
 ls
-grep -RiIl 'HOST_NAME' | xargs sed -i "s/HOST_NAME/$host_name/g"
+grep -RiIl 'FRONT_HOST_NAME' | xargs sed -i "s/FRONT_HOST_NAME/$front_host_name/g"
 grep -RiIl 'FRONT_PORT' | xargs sed -i "s/FRONT_PORT/$front_port/g"
 
 #cd user
@@ -50,20 +52,19 @@ sed -i "s/DB_HOST/$db_host/g" application.properties
 sed -i "s/DB_NAME/$db_name/g" application.properties
 sed -i "s/DB_USERNAME/$db_username/g" application.properties
 sed -i "s/DB_PASSWORD/$db_password/g" application.properties
-sed -i "s/HOST_NAME/$host_name/g" application.properties
 sed -i "s/BACK_PORT/$back_port/g" application.properties
 sed -i "s/SECRET/$secret/g" application.properties
 
 # Modify file in front-end
 cd "$FRONT_DIR/src/api"
-sed -i "s/HOST_NAME/$host_name/g" apiConstants.js
+sed -i "s/BACK_HOST_NAME/$back_host_name/g" apiConstants.js
 sed -i "s/BACK_PORT/$back_port/g" apiConstants.js
 
 cd "$FRONT_DIR"
-sed -i "s/HOST_NAME/$host_name/g" webpack.config.dev.js
+sed -i "s/BACK_HOST_NAME/$back_host_name/g" webpack.config.dev.js
 sed -i "s/BACK_PORT/$back_port/g" webpack.config.dev.js
 
-sed -i "s/HOST_NAME/$host_name/g" webpack.config.prod.js
+sed -i "s/BACK_HOST_NAME/$back_host_name/g" webpack.config.prod.js
 sed -i "s/BACK_PORT/$back_port/g" webpack.config.prod.js
 
 # Modify docker-compose.yml file
